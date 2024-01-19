0e9daa25b896cba10eaca6cbf2edd636459b3bee
SoTilted <103329657+SoTilted@users.noreply.github.com>
2024-01-19 19:22:30 +0200

    Add button to have multiple grid sizes
    Modify css to pretty up the UI
    
    Make coloring gradient

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Etch-a-sketch
+A simple sketchpad/Etch-A-Scetch page made for The Odin Project.
+
+# TODO
+Maybe try and implement buttons that change the color you have to one of the main ones and a button that makes you draw with a random color from each box to the next all the while keeping the gradient effect.
--- main.js
+++ main.js
@@ -2,26 +2,47 @@ const N=16
 const screenSize=960
 let container=document.querySelector('.container')
 
-for (let i=0; i<N*N; i++){
-    let grid=document.createElement("div");
-    grid.setAttribute("class","grid-container");
-    grid.setAttribute("id",`${i}`);
-    grid.style.minWidth=`${screenSize/N - 4}px`
-    grid.style.minHeight=`${screenSize/N - 4}px`
-    container.appendChild(grid);
+function createGrid(Size){
+    let boxes=document.getElementsByClassName("grid-container");
 
-}
+    for (let i=0; i<Size*Size; i++){
+        let grid=document.createElement("div");
+        grid.setAttribute("class","grid-container");
+        grid.setAttribute("id",`${i}`);
+        grid.setAttribute('data-value','255')
+        grid.style.minWidth=`${screenSize/Size - 4}px`
+        grid.style.minHeight=`${screenSize/Size - 4}px`
+        container.appendChild(grid);
+    }
 
-const boxes=document.getElementsByClassName("grid-container");
-
-const buttonHovered = (e) => {
-    currentBox=document.getElementById(e.target.id);
-    currentBox.style.backgroundColor='black';
+    const buttonHovered = (e) => {
+        
+        currentBox=document.getElementById(e.target.id);
+        if(parseFloat(currentBox.dataset.value)===0.0){}
+        else{
+            currentBox.dataset.value=parseFloat(currentBox.dataset.value)-25.5
+            currentBox.style.backgroundColor=`rgb(${currentBox.dataset.value},${currentBox.dataset.value},${currentBox.dataset.value})`;
+        }
+       
+    }
+    for (let box of boxes) {
+        box.addEventListener("mouseenter", buttonHovered); 
+      }
     
 }
 
 
-for (let box of boxes) {
-    box.addEventListener("mouseover", buttonHovered);
-    
-  }
+
+
+  const button = document.getElementById('button')
+  button.addEventListener('click',()=>{
+    let size=parseInt(prompt("Choose size of grid!(max size is 100.)"));
+    while( size>100 || size <=0 ){
+        size=parseInt(prompt("Bad size input, please try again."));
+    }
+    container.innerHTML='';
+    createGrid(size);
+  });
+
+
+createGrid(N);
