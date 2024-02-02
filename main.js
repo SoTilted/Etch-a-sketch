d403d839364ddec0038e3c200634d74e416e42c9
SoTilted <103329657+SoTilted@users.noreply.github.com>
2024-02-02 14:49:49 +0200

    Update JS to fix overflow when resizing window
    Change form mouseover to mouseover and mousedown
    
    Update CSS for the above reasons

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
 const N=16
 const screenSize=960
 let container=document.querySelector('.container')
+let mouseCondition=true;
+
+const buttonClickedAndHovered = (e) => {
+  document.onmousedown = () => (mouseCondition=false);
+  document.onmouseup = () => (mouseCondition=true);
+  if(mouseCondition)return;
+  currentBox=document.getElementById(e.target.id);
+  if(parseFloat(currentBox.dataset.value)===0.0){}
+  else{
+      currentBox.dataset.value=parseFloat(currentBox.dataset.value)-25.5
+      currentBox.style.backgroundColor=`rgb(${currentBox.dataset.value},${currentBox.dataset.value},${currentBox.dataset.value})`;
+  }
+}
+
+
 
 function createGrid(Size){
     let boxes=document.getElementsByClassName("grid-container");
-
     for (let i=0; i<Size*Size; i++){
         let grid=document.createElement("div");
         grid.setAttribute("class","grid-container");
         grid.setAttribute("id",`${i}`);
         grid.setAttribute('data-value','255')
-        grid.style.minWidth=`${screenSize/Size - 4}px`
-        grid.style.minHeight=`${screenSize/Size - 4}px`
+        grid.style.boxSizing = 'border-box'
+        grid.style.width=`${100/Size}%`
+        grid.style.height=`${100/Size}%`
         container.appendChild(grid);
     }
-
-    const buttonHovered = (e) => {
-        
-        currentBox=document.getElementById(e.target.id);
-        if(parseFloat(currentBox.dataset.value)===0.0){}
-        else{
-            currentBox.dataset.value=parseFloat(currentBox.dataset.value)-25.5
-            currentBox.style.backgroundColor=`rgb(${currentBox.dataset.value},${currentBox.dataset.value},${currentBox.dataset.value})`;
-        }
-       
-    }
     for (let box of boxes) {
-        box.addEventListener("mouseenter", buttonHovered); 
-      }
-    
+      box.addEventListener("mouseover", buttonClickedAndHovered); 
+    }
 }
-
-
-
-
   const button = document.getElementById('button')
   button.addEventListener('click',()=>{
     let size=parseInt(prompt("Choose size of grid!(max size is 100.)"));
@@ -43,6 +42,5 @@ function createGrid(Size){
     container.innerHTML='';
     createGrid(size);
   });
-
 
 createGrid(N);
