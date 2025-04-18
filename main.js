6080219552a080d1425d92ae2865864b6c06d5cd
SoTilted <103329657+SoTilted@users.noreply.github.com>
2025-04-18 21:22:22 +0300

    Completes TODO task.  - Makes it responsive on touch screen devices.

--- main.js
+++ main.js
@@ -1,119 +1,148 @@
-const N=16
-const screenSize=960
-let container=document.querySelector('.container')
-let mouseCondition=true;
-
+const N = 16;
+let container = document.querySelector('.container');
+let mouseCondition = false;
+let lastTouchedBox = null;
+document.onmousedown = () => (mouseCondition = true);
+document.onmouseup = () => (mouseCondition = false);
+document.ontouchend = () => (lastTouchedBox = null);
 const buttonClickedAndHovered = (e) => {
-  document.onmousedown = () => (mouseCondition=false);
-  document.onmouseup = () => (mouseCondition=true);
-  if(mouseCondition)return;
-  currentBox=document.getElementById(e.target.id);
-  rgbValues=currentBox.dataset.value.split(',')
-  switch (colour.textContent){
+  currentBox = document.getElementById(e.target.id);
+  rgbValues = currentBox.dataset.value.split(',');
+  switch (colour.textContent) {
     case 'Black':
-      if(parseFloat(rgbValues[0])===0.0 && parseFloat(rgbValues[1])===0.0 && parseFloat(rgbValues[2])===0.0){break;}
-      else{
-        for(let i=0; i<3; i++){
-          if(parseFloat(rgbValues[i])!=0.0){
-            rgbValues[i]=rgbValues[i]-25.5;
+      if (
+        parseFloat(rgbValues[0]) === 0.0 &&
+        parseFloat(rgbValues[1]) === 0.0 &&
+        parseFloat(rgbValues[2]) === 0.0
+      ) {
+        break;
+      } else {
+        for (let i = 0; i < 3; i++) {
+          if (parseFloat(rgbValues[i]) != 0.0) {
+            rgbValues[i] = rgbValues[i] - 25.5;
           }
         } 
-        currentBox.dataset.value=rgbValues
-        currentBox.style.backgroundColor=`rgb(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]})`;
+        currentBox.dataset.value = rgbValues;
+        currentBox.style.backgroundColor = `rgb(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]})`;
         break;
       }
     case 'Red':
-      if(parseFloat(rgbValues[0])===255.0 && parseFloat(rgbValues[1])===0.0 && parseFloat(rgbValues[2])===0.0){break;}
-      else{
-        for(let i=1; i<3; i++){
-          if(parseFloat(rgbValues[i])!=0.0){
-            rgbValues[i]=rgbValues[i]-25.5;
+      if (
+        parseFloat(rgbValues[0]) === 255.0 &&
+        parseFloat(rgbValues[1]) === 0.0 &&
+        parseFloat(rgbValues[2]) === 0.0
+      ) {
+        break;
+      } else {
+        for (let i = 1; i < 3; i++) {
+          if (parseFloat(rgbValues[i]) != 0.0) {
+            rgbValues[i] = rgbValues[i] - 25.5;
           }
         } 
-        currentBox.dataset.value=rgbValues
-        currentBox.style.backgroundColor=`rgb(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]})`;
+        currentBox.dataset.value = rgbValues;
+        currentBox.style.backgroundColor = `rgb(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]})`;
         break;
       }
     case 'Green':
-      if(parseFloat(rgbValues[0])===0.0 && parseFloat(rgbValues[1])===255.0 && parseFloat(rgbValues[2])===0.0){break;}
-      else{
-        for(let i=0; i<3; i+=2){
-          if(parseFloat(rgbValues[i])!=0.0){
-            rgbValues[i]=rgbValues[i]-25.5;
+      if (
+        parseFloat(rgbValues[0]) === 0.0 &&
+        parseFloat(rgbValues[1]) === 255.0 &&
+        parseFloat(rgbValues[2]) === 0.0
+      ) {
+        break;
+      } else {
+        for (let i = 0; i < 3; i += 2) {
+          if (parseFloat(rgbValues[i]) != 0.0) {
+            rgbValues[i] = rgbValues[i] - 25.5;
           }
         } 
-        currentBox.dataset.value=rgbValues
-        currentBox.style.backgroundColor=`rgb(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]})`;
+        currentBox.dataset.value = rgbValues;
+        currentBox.style.backgroundColor = `rgb(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]})`;
         break;
       }
     case 'Blue':
-      if(parseFloat(rgbValues[0])===0.0 && parseFloat(rgbValues[1])===0.0 && parseFloat(rgbValues[2])===255.0){break;}
-      else{
-        for(let i=0; i<2; i++){
-          if(parseFloat(rgbValues[i])!=0.0){
+      if (
+        parseFloat(rgbValues[0]) === 0.0 &&
+        parseFloat(rgbValues[1]) === 0.0 &&
+        parseFloat(rgbValues[2]) === 255.0
+      ) {
+        break;
+      } else {
+        for (let i = 0; i < 2; i++) {
+          if (parseFloat(rgbValues[i]) != 0.0) {
             console.log(rgbValues[i]);
-            rgbValues[i]=rgbValues[i]-25.5;
+            rgbValues[i] = rgbValues[i] - 25.5;
           }
         } 
-        currentBox.dataset.value=rgbValues
-        currentBox.style.backgroundColor=`rgb(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]})`;
+        currentBox.dataset.value = rgbValues;
+        currentBox.style.backgroundColor = `rgb(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]})`;
         break;
       }
     
     case 'White':
-      rgbValues=[255,255,255]
-      currentBox.dataset.value=rgbValues
-      currentBox.style.backgroundColor=`rgb(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]})`;
+      rgbValues = [255, 255, 255];
+      currentBox.dataset.value = rgbValues;
+      currentBox.style.backgroundColor = `rgb(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]})`;
   }
-}
+};
 
-function createGrid(Size){
-    let boxes=document.getElementsByClassName("grid-container");
-    for (let i=0; i<Size*Size; i++){
-        let grid=document.createElement("div");
-        grid.setAttribute("class","grid-container");
-        grid.setAttribute("id",`${i}`);
-        grid.setAttribute('data-value',[255,255,255])
-        grid.style.boxSizing = 'border-box'
-        grid.style.width=`${100/Size}%`
-        grid.style.height=`${100/Size}%`
+function createGrid(Size) {
+  let boxes = document.getElementsByClassName('grid-container');
+  for (let i = 0; i < Size * Size; i++) {
+    let grid = document.createElement('div');
+    grid.setAttribute('class', 'grid-container');
+    grid.setAttribute('id', `${i}`);
+    grid.setAttribute('data-value', [255, 255, 255]);
+    grid.style.boxSizing = 'border-box';
+    grid.style.width = `${100 / Size}%`;
+    grid.style.height = `${100 / Size}%`;
         container.appendChild(grid);
     }
     for (let box of boxes) {
-      box.addEventListener("mouseover", buttonClickedAndHovered); 
-    }
+    box.addEventListener('mouseover', (e) => {
+      if (mouseCondition) buttonClickedAndHovered(e);
+    });
+    box.addEventListener('touchmove', (e) => {
+      const touch = e.touches[0];
+      const element = document.elementFromPoint(touch.clientX, touch.clientY);
+      if (element && element.classList.contains('grid-container')) {
+        if (element !== lastTouchedBox) {
+          lastTouchedBox = element;
+          buttonClickedAndHovered({ target: element });
+        }
+      }
+      console.log(touch);
+    });
+  }
 }
 
-let buttons = document.getElementsByTagName('button')
-for (let button of buttons){
-  if (button.id==='grid'){
-    button.addEventListener('click',()=>{
-      let size=parseInt(prompt("Choose size of grid!(max size is 100.)"));
-      while( size>100 || size <=0 || isNaN(size)){
-        size=parseInt(prompt("Bad size input, please try again."));
+let buttons = document.getElementsByTagName('button');
+for (let button of buttons) {
+  if (button.id === 'grid') {
+    button.addEventListener('click', () => {
+      let size = parseInt(prompt('Choose size of grid!(max size is 100.)'));
+      while (size > 100 || size <= 0 || isNaN(size)) {
+        size = parseInt(prompt('Bad size input, please try again.'));
       }
-      container.innerHTML='';
+      container.innerHTML = '';
       createGrid(size);
     });
-  }
-  else if(button.id==='colour'){
+  } else if (button.id === 'colour') {
     let counter = 1;
-    coloursList=['Black','Red','Green','Blue','White']
-    //const colour = document.getElementById('colour');
-    button.style.backgroundColor='black'
-    button.addEventListener('click',()=>{
-      button.textContent=coloursList[counter%5];
-      button.style.backgroundColor=`${coloursList[counter%5]}`
-      button.style.color=`${coloursList[counter%5]}`
+    coloursList = ['Black', 'Red', 'Green', 'Blue', 'White'];
+    button.style.backgroundColor = 'black';
+    button.addEventListener('click', () => {
+      button.textContent = coloursList[counter % 5];
+      button.style.backgroundColor = `${coloursList[counter % 5]}`;
+      button.style.color = `${coloursList[counter % 5]}`;
       counter++;
     });
-  }
-  else{
-    button.addEventListener('click',()=>{
-      container.innerHTML='';
+  } else {
+    button.addEventListener('click', () => {
+      container.innerHTML = '';
       createGrid(N);
     });     
-  };
+  }
 }
 
 createGrid(N);
